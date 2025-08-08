9fb6174700f3c50bbdc2fd6df93e2fc2b6a87d4c
chisschwartz <christopherschwartz559@gmail.com>
2025-08-07 19:00:39 -0500

    working through more problems

--- solutions/Problem-6.js
+++ solutions/Problem-6.js
@@ -99,6 +99,7 @@ var productExceptSelf = function(nums) {
 
     const n = nums.length;
     const res = new Array(n).fill(1);
+    //creates a new array filled with 1s
 
     for (let i = 1; i < n; i++) {
         res[i] = res [i - 1] * nums[i - 1];
